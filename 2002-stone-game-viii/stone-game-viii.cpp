eb37a07505db68ddf46e6ebcab5df1f5e5dc8f09
Harshvardhan Singh <hvsingh46@gmail.com>
2026-08-24 15:13:51 +0530

    Time: 102 ms (73.21%) | Memory: 87.1 MB (96.05%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i < n; i++) { //in place shi to save space ;)
             stones[i] += stones[i - 1];
         }
         int maxi = stones[n - 1];
         for(int i = n - 2; i >= 1; i--) {
             maxi = max(maxi, stones[i] - maxi);
-        }
+        } // Each iteration would update the maximum answer possible
         return maxi;
     }
 };
