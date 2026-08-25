3a7b7e3196a01aa3f1b21028ff864c49702b674b
Harshvardhan Singh <hvsingh46@gmail.com>
2026-08-26 01:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,28 +1,11 @@
 class Solution {
-private:
-    int helper(vector<int>& nums, int l, int r) {
-        if(l == r) return nums[l];
-        // here bs = border sum :d
-        int maxlbs = INT_MIN, maxrbs = INT_MIN, mid = l + ((r - l) >> 1);
-        //RE calls for the left and right part
-        int mls = helper(nums, l, mid);
-        int mrs = helper(nums, mid + 1, r);
-        //Max Cross Border Sum for either Left crossborder or right crossborder
-        int lbs = 0, rbs = 0;
-        for(int i = mid; i >= l; i--) {
-            lbs += nums[i];
-            if(lbs > maxlbs) maxlbs = lbs;
-        }
-        for(int i = mid+1; i <= r; i++) {
-            rbs += nums[i];
-            if(rbs > maxrbs) maxrbs = rbs;
-        }
-        int cbs = maxlbs + maxrbs; //cbs: Cross Border Sum
-        return max(mls, max(mrs, cbs));
-    }
-
 public:
     int maxSubArray(vector<int>& nums) {
-        return helper(nums, 0, nums.size() - 1);
+     int c = nums[0], ans = nums[0]; // c is current 
+     for(int i = 1; i < nums.size(); i++) {
+        c = max(nums[i], c + nums[i]);
+        ans = max(c, ans);
+     }
+     return ans;
     }
 };
