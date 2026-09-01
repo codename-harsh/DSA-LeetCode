ad18835fbc8ca166799c882ce9b161206572603f
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-01 22:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (99.81%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,8 +4,6 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         int i = 0, j = 0, m1 = 0, m2 = 0;
-
-        // Find median.
         for (int count = 0; count <= (n + m) / 2; count++) {
             m2 = m1;
             if (i != n && j != m) {
@@ -20,8 +18,6 @@ public:
                 m1 = nums2[j++];
             }
         }
-
-        // Check if the sum of n and m is odd.
         if ((n + m) % 2 == 1) {
             return static_cast<double>(m1);
         } else {
