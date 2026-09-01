cc91dc77ce8c5ebf6ff4ca78b7d91ea4ad0651a8
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-02 01:05:51 +0530

    Time: 7 ms (17.61%) | Memory: 18.1 MB (9.70%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
+        if(k == 0)
+            return 0;
         unordered_map<int,int> freq;
         int ans = 0;
         for(int x : nums){
