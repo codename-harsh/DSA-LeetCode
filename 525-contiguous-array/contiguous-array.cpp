03ce42e2cc8a5b9da088f4e9f782769c2fdcabe9
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-01 14:27:35 +0530

    Time: 36 ms (10.58%) | Memory: 88.3 MB (43.12%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -5,8 +5,10 @@ public:
         unordered_map<int, int> seen{{0, -1}};
         for(int i=0; i<nums.size(); i++){
             sum += nums[i]==1 ? 1 : -1;
-            if(seen.count(sum)) maxLen = max(maxLen, i-seen[sum]);
-            else seen[sum] = i;
+            if(seen.count(sum))
+            maxLen = max(maxLen, i-seen[sum]);
+            else 
+                seen[sum] = i;
         }
         return maxLen;
     }
