9b79294cb97bcb115296cba2349cff49bb8ca137
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-09 21:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (84.09%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int maxi = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if(ch == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if(!st.empty()) {
+                    int len = i - st.top();
+                    maxi = max(maxi, len);
+                } else {
+                    st.push(i);
+                }
+            }
+        }
+        return maxi;
+    }
+};
