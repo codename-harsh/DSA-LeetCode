a382f79a1c0cb30cc5d548f7f028ad8aee461005
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-12 18:39:24 +0530

    Time: 7 ms (50.91%) | Memory: 14.2 MB (49.66%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    //BIG POTATO STUFF MUHEHEHEHE
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(const auto&x: s) {
+            if(!st.empty() && st.top() == x) {
+                st.pop(); // we dont want this guy
+            } else {
+                st.push(x); // yeah we love this one :D
+            }
+        }
+        //add the stack guys to string and then reverse to get answer !
+        string ans;
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+
+    // SMALL POTATO STUFF (BASIC APPROACH)
+    
+    // string removeDuplicates(string s) {
+    //         int i = 0;
+    //         string ans = "";
+    //         while(i < s.length())
+    //         {
+    //             if(ans.length() > 0)
+    //             {
+    //                 if(ans[ans.length()-1] == s[i]){
+    //                     ans.pop_back();
+    //                 }
+    //                 else{
+    //                     ans.push_back(s[i]);
+    //                 }
+    //             }
+    //                 else{ //If ans is empty then directly insert the first element of s
+    //                     ans.push_back(s[i]);
+    //                 }
+    //                 i++;
+    //         }
+    //         return ans;
+    // }
+};
