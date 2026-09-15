89eaba36debb034fb191a071e5477b6f796f3008
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-15 23:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.43%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -3,18 +3,19 @@ private:
     const bool isPal(string&s, int l, int r) {
         for( ; l < r; l++, r--) {
             if(s[l] != s[r]) return false;
-        }
-        return true;
+        } return true; //else true !
     }
 
 public:
     int maxPalindromes(string s, int k) {
         int c = 0, nxt = 0;
         for(int r = k -1; r < s.length(); r++) {
-            int l = r - k + 1;
+            int l = r - k + 1; //size of the dynamic sliding window we gonna use 
             bool add = (l >= nxt && isPal(s, l, r)) || (l>nxt && isPal(s, l - 1, r));
+            if(add) {
             c += add;
-            if(add) nxt = r + 1;
+                nxt = r + 1;
+            }
         }
         return c;
     }
