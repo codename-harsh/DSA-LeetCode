bf4b6bc9eadefe9d57e6a1467f5d7c93150e23fc
Harshvardhan Singh <hvsingh46@gmail.com>
2026-09-18 01:37:02 +0530

    Time: 4 ms (89.06%) | Memory: 88.8 MB (93.38%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int ans = INT_MAX, n = arr.size(), l = 0, sum = 0;
-        vector<int> v(n, INT_MAX);
+        vector<int> v(n, INT_MAX);//index 0 se i tak jo target-sum subarray mila hai, uski minimum length kya hai?
         for(int r = 0; r < n; r++) {
             sum += arr[r];
             while(sum > target) {
                 sum -= arr[l++];
             }
             if(sum == target) {
-                int curr = r - l + 1;
-                if(l > 0 && v[l - 1] != INT_MAX) {
+                int curr = r - l + 1;//length of the current window 
+                if(l > 0 && v[l - 1] != INT_MAX) { // finding minimum length 
                     ans = min(ans, curr + v[l - 1]);
                 }
-                v[r] = curr;
+                v[r] = curr;//updation of tracks
             }
-            if(r > 0) {
+            if(r > 0) {//Agar current index pe naya target subarray nahi mila, toh previous best ko aage le jao
                 v[r] = min(v[r], v[r-1]);
             }
         }
